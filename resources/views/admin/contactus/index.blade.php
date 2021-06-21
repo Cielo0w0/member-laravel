5ec048d4ee786aad8d5c0c9aff6854978dbdfbc8
Cielo0w0 <82428851+Cielo0w0@users.noreply.github.com>
2021-06-21 17:09:46 +0800

    20210621-17.21

--- resources/views/admin/contactus/index.blade.php
+++ resources/views/admin/contactus/index.blade.php
@@ -4,79 +4,83 @@
 
 
 @section('css')
-    <style>
-        .card-header h2{
-            margin-bottom: 0
-        }
-    </style>
+<style>
+    .card-header h2 {
+        margin-bottom: 0
+    }
+</style>
 @endsection
 
 @section('content')
-    <div class="container">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{asset('/admin/home')}}">首頁</a></li>
-                <li class="breadcrumb-item active" aria-current="page">聯絡我們管理</li>
-            </ol>
-        </nav>
+<div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{asset('/admin/home')}}">首頁</a></li>
+            <li class="breadcrumb-item active" aria-current="page">聯絡我們管理</li>
+        </ol>
+    </nav>
 
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>聯絡我們管理</h2>
-                    </div>
-
-
-                    <div class="card-body">
-
-                        <table id="my-datatable" class="display" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>姓名</th>
-                                    <th>信箱</th>
-                                    <th>主旨</th>
-                                    <th>操作</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                @foreach ($lists as $item )
-                                    <tr>
-                                        <td>{{ $item->name }}</td>
-                                        <td>{{ $item->email }}</td>
-                                        <td>{{ $item->title }}</td>
-                                        <td>
-                                            <a class="btn btn-primary btn-sm"
-                                                href="{{ asset('/admin/contactus/seemore) }}/{{$item->id}}">查看
-                                            </a>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h2>聯絡我們管理</h2>
+                </div>
 
 
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <th>姓名</th>
-                                    <th>信箱</th>
-                                    <th>主旨</th>
-                                    <th>操作</th>
-                                </tr>
-                            </tfoot>
-                        </table>
+                <div class="card-body">
 
-                    </div>
+                    <table id="my-datatable" class="display" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>姓名</th>
+                                <th>信箱</th>
+                                <th>主旨</th>
+                                <th>操作</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach ($lists as $item )
+                            <tr>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->email }}</td>
+                                <td>{{ $item->title }}</td>
+                                <td>
+                                    <a class="btn btn-primary btn-sm"
+                                        href="{{ asset('/admin/news/edit') }}/{{$item->id}}">編輯</a>
+
+                                    <form style="display: inline-block"
+                                        action="{{ asset('/admin/news/delete') }}/{{$item->id}}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-danger btn-sm">刪除</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>姓名</th>
+                                <th>信箱</th>
+                                <th>主旨</th>
+                                <th>操作</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('js')
-    <script>
-        $(document).ready(function() {
-            $('#my-datatable').DataTable();
-        });
-    </script>
+<script>
+    $(document).ready(function() {
+    $('#my-datatable').DataTable();
+});
+</script>
 @endsection
